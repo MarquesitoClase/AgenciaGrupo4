24576dda742231cb813d2482b547b5af347af661
David-Navarro-Oliver <navarropalle@gmail.com>
2025-12-10 00:31:11 +0100

    feat: initialize cruises hero portal with image mappings and element references

--- js/cruises.js
+++ js/cruises.js
@@ -21,3 +21,29 @@ document.addEventListener("DOMContentLoaded", () => {
       navLinks.classList.toggle("is-open");
     });
   }
+
+  const heroStage = document.querySelector(".heroCruisesStage");
+  const baseImage = document.querySelector(".heroBase");
+  const activeImage = document.querySelector(".heroActive");
+  const holdButton = document.getElementById("travelHoldButton");
+
+  if (heroStage && baseImage && activeImage && holdButton) {
+    const keys = [
+      "cerdena",
+      "fiordos",
+      "inglaterra",
+      "francia",
+      "grecia",
+      "tanger",
+      "caribe",
+    ];
+
+    const images = {
+      cerdena: "../media/images/cerdena.jpg",
+      fiordos: "../media/images/fiordos.jpg",
+      inglaterra: "../media/images/inglaterra.jpg",
+      francia: "../media/images/francia.jpg",
+      grecia: "../media/images/grecia.jpg",
+      tanger: "../media/images/tanger.jpg",
+      caribe: "../media/images/caribe.jpg",
+    };
