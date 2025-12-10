5cefd7e417e0636166bed3c13c4d0b4032969588
MarquesitoClase <juan.luis.marquezcan@gmail.com>
2025-12-10 10:19:01 +0100

    created oferts.js

---
+++ js/oferts.js
@@ -0,0 +1,15 @@
+let place = document.getElementById("ofertsView");
+
+let jsonOfertas = [
+    {
+        key: "fiordos",
+        image: "../media/images/fiordos.jpg",
+        alt: "Fiordo noruego con montañas nevadas",
+        title: "Fiordos Noruegos",
+        meta: "7 noches · salida desde Copenhague",
+        text: "Ruta: Copenhague · Bergen · Geiranger · Hellesylt · Stavanger. Montañas infinitas, cascadas gigantes y una navegación única entre cañones naturales de hielo y roca.",
+        precio: 70,
+        plazasDisponibles: 4
+    }
+]
+
